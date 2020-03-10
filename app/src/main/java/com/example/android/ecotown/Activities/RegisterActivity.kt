02dee9741f44b0c7e52c162f94c50240b36d834d
VeraUvads <vero971410@gmail.com>
2020-03-10 16:14:14 +0700

    Register layout

--- app/src/main/java/com/example/android/ecotown/Activities/RegisterActivity.kt
+++ app/src/main/java/com/example/android/ecotown/Activities/RegisterActivity.kt
@@ -1,12 +1,13 @@
-package com.example.android.ecotown
+package com.example.android.ecotown.Activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.android.ecotown.R
 
-class MainActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_register)
     }
 }
