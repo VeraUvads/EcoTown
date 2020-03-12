e058bfc8643933a529a67a7231770a6e3d2de145
VeraUvads <vero971410@gmail.com>
2020-03-12 23:27:39 +0700

    Update Nav Header

--- app/src/main/java/com/example/android/ecotown/activities/HomeActivity.kt
+++ app/src/main/java/com/example/android/ecotown/activities/HomeActivity.kt
@@ -6,32 +6,47 @@ import android.renderscript.ScriptGroup
 import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.example.android.ecotown.R
 import com.example.android.ecotown.databinding.ActivityHomeBinding
 import com.example.android.ecotown.databinding.ContentMainBinding
+import com.example.android.ecotown.databinding.NavHeaderBinding
 import com.example.android.ecotown.fragment.HomeFragment
 import com.example.android.ecotown.fragment.TrackFragment
 import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.android.synthetic.main.nav_header.view.*
 
 
 class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
-    lateinit var bindingHome: ActivityHomeBinding
-    lateinit var bindingContent: ContentMainBinding
+    private lateinit var bindingHome: ActivityHomeBinding
+    private lateinit var bindingContent: ContentMainBinding
 
-    lateinit var drawerLayout: DrawerLayout
-    lateinit var navView: NavigationView
+    lateinit var mAuth: FirebaseAuth
+
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navView: NavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         bindingHome = ActivityHomeBinding.inflate(layoutInflater)
         bindingContent = ContentMainBinding.inflate(layoutInflater)
+
         setContentView(bindingHome.root)
 
-        var toolbar = bindingContent.toolbar
+        mAuth = FirebaseAuth.getInstance()
+        val currentUser = mAuth?.currentUser
+        navHeader(currentUser)
+
+        var toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, HomeFragment()).commit()
 
         drawerLayout = bindingHome.drawerLayout
         navView = bindingHome.navView
@@ -40,12 +55,24 @@ class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         )
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
-        navView.setNavigationItemSelectedListener(this)
 
+        navView.setNavigationItemSelectedListener(this)
+    }
+
+    private fun navHeader(currentUser: FirebaseUser?) {
+        val navView: NavigationView = bindingHome.navView
+        val headerView = navView.getHeaderView(0)
+        val navName = headerView.nav_user_name
+        val navMail = headerView.nav_user_mail
+
+        navMail.text = currentUser?.email
+        navName.text = currentUser?.displayName
 
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
+
+        supportActionBar?.title = item.title
         when (item.itemId) {
             R.id.nav_home -> {
                 supportFragmentManager.beginTransaction()
--- app/src/main/java/com/example/android/ecotown/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/android/ecotown/fragment/HomeFragment.kt
@@ -1,2 +1,27 @@
 package com.example.android.ecotown.fragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.android.ecotown.R
+import com.example.android.ecotown.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    lateinit var binding: FragmentHomeBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentHomeBinding.inflate(layoutInflater)
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
